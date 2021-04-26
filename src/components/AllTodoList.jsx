18c2926cb3bd1693026e2d224a310594ac38190d
MD ARIF HOSSAIN <arif.swfu@outlook.com>
2021-04-26 19:54:22 +0800

    run on android

--- src/components/AllTodoList.jsx
+++ src/components/AllTodoList.jsx
@@ -1,12 +1,42 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from "react";
+import {  StyleSheet, View, Text, TextInput } from "react-native";
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    // fontFamily: "Comic Sans MS",
+    borderRadius: 25,
+    paddingLeft: 20,
+  },
+});
 
 const AllTodoList = () => {
-    return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>AllTodoList</Text>
-        </View>
-    )
-}
+  const [text, onChangeText] = React.useState("");
 
-export default AllTodoList
+  return (
+    // <SafeAreaView>
+      <View style={{ flex: 1, marginTop: 50 }}>
+        <Text
+          style={{
+            textAlign: "center",
+            fontSize: 20,
+            fontWeight: "bold",
+            // fontFamily: "Comic Sans MS",
+          }}
+        >
+          All Todo List
+        </Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeText}
+          value={text}
+          placeholder="What needs to be done?"
+        />
+      </View>
+    // </SafeAreaView>
+  );
+};
+
+export default AllTodoList;
